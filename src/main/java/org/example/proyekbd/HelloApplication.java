854b24b9c8dcd88483c3bd6feb975d08ef94349d
Wolf-web-dot <wolfydragneel740@gmail.com>
2025-06-06 16:31:38 +0900

    tes

--- src/main/java/org/example/proyekbd/HelloApplication.java
+++ src/main/java/org/example/proyekbd/HelloApplication.java
@@ -18,6 +18,8 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+        System.out.println( "Hello World!");
+        System.out.println("hail");
         launch();
     }
 }
