6a35f5294a2967ea9bdaf5f39a00e2a901f87673
SEANVSA <seanvs06@gmail.com>
2025-06-06 15:18:28 +0700

    Punya koko ku copy

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,17 @@
-module org.example.proyekbd {
+module com.example.bdsqltester {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.zaxxer.hikari;
+    requires java.sql;
+    requires org.slf4j;
 
 
-    opens org.example.proyekbd to javafx.fxml;
-    exports org.example.proyekbd;
+    opens com.example.bdsqltester to javafx.fxml;
+    exports com.example.bdsqltester;
+    exports com.example.bdsqltester.datasources;
+    opens com.example.bdsqltester.datasources to javafx.fxml;
+    exports com.example.bdsqltester.scenes;
+    opens com.example.bdsqltester.scenes to javafx.fxml;
+    exports com.example.bdsqltester.scenes.admin;
+    opens com.example.bdsqltester.scenes.admin to javafx.fxml;
 }
